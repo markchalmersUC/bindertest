2ec8c8baa162c06eebc9f76aa6ab128d76b2eaff
markchalmersUC <48764799+markchalmersUC@users.noreply.github.com>
2019-03-29 08:50:22 -0400

    Add files via upload

---
+++ datadayworkshop/install.R
@@ -0,0 +1,7 @@
+install.packages("tidyverse")
+install.packages("tabularizer")
+install.packages("dplyr")
+install.packages("sf")
+install.packages('leaflet')
+install.packages("tmap")
+install.packages('tigris')
